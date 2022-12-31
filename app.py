13f509ce7099e399a3b4705c5865ab489c8a2a53
Durgesh Choudhari <32238567+DurgeshChoudhari@users.noreply.github.com>
2022-12-31 23:02:28 +0530

    Search by Date

--- app.py
+++ app.py
@@ -2,14 +2,21 @@ import streamlit as st
 import pandas as pd
 import preprocessor, helper
 import matplotlib.pyplot as plt
+import datetime
+
+st.set_page_config(page_title='WhatsApp Chat Analyzer', page_icon="📊", layout="centered", initial_sidebar_state="auto", menu_items=None)
 
 st.sidebar.title("WhatsApp Chat Analyzer")
+st.title('Welcome to WhatsApp Chat Analyzer')
+# st.write('Please select the Individual/Group chat text file')
+# st.image('what.jfif', width=750)
 
 uploaded_file = st.sidebar.file_uploader("Choose a file")
 if uploaded_file is not None:
     # To read file as bytes:
     data = uploaded_file.getvalue().decode("utf-8")
     # st.write(bytes_data)
+
     df = preprocessor.preprocess(data)
     # st.dataframe(df)
 
@@ -25,6 +32,7 @@ if uploaded_file is not None:
     num_msgs, words, media, urls, most_common_word = helper.fetch_stats(selected_user, df)
     em_df, emojis = helper.emoji_helper(selected_user, df)
     monthly_timeline, daily_timeline, day_timeline = helper.timeline_helper(selected_user,df)
+
     	
     if st.sidebar.button("Show analysis"):
     	st.header('Total')
@@ -77,38 +85,6 @@ if uploaded_file is not None:
 	    	plt.xticks(rotation='vertical')
 	    	st.pyplot(fig7)
 
-
-
-		# col1, col2 = st.columns(2)
-    	# with col1:
-    	# 	st.subheader('Monthly Timeline')
-    	# 	fig4, ax4 = plt.subplots()
-	    # 	ax4.plot(monthly_timeline['time'], monthly_timeline['messages'])
-	    # 	plt.xticks(rotation='vertical')
-	    # 	st.pyplot(fig4)
-
-    	# with col2:
-	    # 	st.subheader('Daily Timeline')
-	    # 	fig5, ax5 = plt.subplots()
-	    # 	ax5.plot(daily_timeline['dt'], daily_timeline['messages'])
-	    # 	plt.xticks(rotation='vertical')
-	    # 	st.pyplot(fig5)
-
-
-	    # col1, col2 = st.columns(2)
-    	# with col1:
-    	# 	fig6, ax6 = plt.subplots()
-	    # 	ax6.bar(day_timeline['index'], day_timeline['day_name'])
-	    # 	plt.xticks(rotation='vertical')
-	    # 	st.pyplot(fig6)
-		# with col2:
-	    # 	fig7, ax7 = plt.subplots()
-	    # 	ax7.bar(monthly_timeline['month'], monthly_timeline['messages'])
-	    # 	plt.xticks(rotation='vertical')
-	    # 	st.pyplot(fig7)
-
-	    
-
     	st.title('Most Busiest')
     	x,new_df = helper.most_busiest_person(df)
     	fig, ax = plt.subplots()
@@ -140,28 +116,9 @@ if uploaded_file is not None:
     		ax3.pie(em_df[1].head(10), labels = em_df[0].head(10), autopct="%0.2f")
     		plt.xticks(rotation='vertical')
     		st.pyplot(fig3)
-    	# # st.dataframe(monthly_timeline)
-    	# fig4, ax4 = plt.subplots()
-    	# ax4.plot(monthly_timeline['time'], monthly_timeline['messages'])
-    	# plt.xticks(rotation='vertical')
-    	# st.pyplot(fig4)
-
-    	# # st.dataframe(daily_timeline)
-    	# fig5, ax5 = plt.subplots()
-    	# ax5.plot(daily_timeline['dt'], daily_timeline['messages'])
-    	# plt.xticks(rotation='vertical')
-    	# st.pyplot(fig5)
-
-    	# # st.dataframe(day_timeline)
-    	# fig6, ax6 = plt.subplots()
-    	# ax6.bar(day_timeline['index'], day_timeline['day_name'])
-    	# plt.xticks(rotation='vertical')
-    	# st.pyplot(fig6)
-
-
-    	# st.dataframe(monthly_timeline)
-    	# fig7, ax7 = plt.subplots()
-    	# ax7.bar(monthly_timeline['month'], monthly_timeline['messages'])
-    	# plt.xticks(rotation='vertical')
-    	# st.pyplot(fig7)
-    	# 
+    # st.success('Click on show Analysis for analysis')
+    st.subheader('Search by Date')
+    date_list = df['dt'].unique().tolist()
+    selected_date = st.selectbox("Select date", date_list)
+    d_df = df[df['dt']==selected_date]
+    st.dataframe(d_df.set_index('date')[[ 'user', 'messages']])
